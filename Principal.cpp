d3a3e80c2027fed6db7ae409cacea3e0fb270a5b
Axel Alvarez <axjavier4alvarez44@gmail.com>
2024-04-17 00:37:39 -0600

    CRUD AGREGAR Y EDITAR

--- Principal.cpp
+++ Principal.cpp
@@ -14,14 +14,12 @@ struct Traductor{
 	char funcionalidad[250];
 };
 
+/* DECLARAR LOS METODOS LEER, CREAR, ACTUALIZAR Y BORRAR*/
 // metodo de leer
 void Leer();
 // metodo
 void Crear();
 
-void Actualizar();
-
-void Borrar();
 
 // MENU CON LAS OPCIONES ORDENADAS
 main(){
@@ -46,10 +44,10 @@ main(){
                 Crear();
                 break;
             case 3:
-                Actualizar();
+                // Actualizar();
                 break;
             case 4:
-                Borrar();
+                // Borrar();
                 break;
             case 0:
                 cout << "Ha salido del programa";
@@ -141,92 +139,3 @@ void Crear(){
 	Leer();
 }
 
-void Actualizar(){
-	// r+b abre fichero para escritura, actualizar crea y borra
-	FILE * archivo = fopen(nombre_archivo, "r+b");
-	// declarar
-	Traductor traductor;
-	
-	// preguntarle por el id
-	int id=0;
-	
-	Leer(); 
-	
-	cout<<"Ingrese el ID que desea Modificar: ";
-	cin>>id;
-	// posicionar mi puntero fseek para posicionar puntero en una determinada posicion
-	
-	// BUSCAR POR EL ID
-	fseek(archivo,id * sizeof(Traductor), SEEK_SET);
-	
-		cin.ignore();
-		
-		// Crear(); y actualizar();
-		
-		// necesitamos el get de todo
-		cout<<"Ingrese la Palabra: ";
-		cin.getline(traductor.palabras, 20);
-		
-		cout<<"Ingrese la traduccion de la palabra: ";
-		cin.getline(traductor.traduccion, 35);
-		
-		cout<<"Ingrese su Funcionalidad: ";
-		cin.getline(traductor.funcionalidad, 250);
-		
-		// tenemos que escribir los registros
-		// bandera default 1
-		fwrite(&traductor,sizeof(Traductor), 1, archivo);
-	// cerrar el archivo
-	fclose(archivo);	
-	// mandar a llamar a leer
-	Leer();
-}
-
-void Borrar(){
-	const char * nombre_archivo_temp = "archivo_temp.dat";
-	FILE * archivo = fopen(nombre_archivo, "rb");
-	FILE * archivo_temp = fopen(nombre_archivo_temp, "w+b");
-	
-	Traductor traductor;
-	
-	int id=0, id_n=0;
-	
-	cout<<"Ingrese el ID a eliminar:";
-	cin>>id;
-	
-	// recorrer el archivo principal
-	while(fread(&traductor,sizeof(Traductor), 1, archivo)){
-		
-	// en el archivo nuevo agrego todos
-	// menos el que quiero que se elimine
-		if(id_n != id){
-		    fwrite(&traductor,sizeof(Traductor), 1, archivo_temp);
-
-		}
-	// se incrementa
-		id_n++;
-	}
-	
-    // archivo extra para eliminar algun dato
-	fclose(archivo);
-	fclose(archivo_temp);
-	
-	// tiene los datos que quiero en el original
-	archivo_temp = fopen(nombre_archivo_temp, "rb");
-	// luego escribir dentro del archivo permanente
-	archivo = fopen(nombre_archivo, "wb");
-	
-	// recorrido a archivo_temp
-	while(fread(&traductor,sizeof(Traductor), 1, archivo_temp)){
-		
-		// escribo en mi archivo
-		    fwrite(&traductor,sizeof(Traductor), 1, archivo);
-}
-
-    fclose(archivo);
-	fclose(archivo_temp);
-	
-	
-	Leer(); 
-
-}
