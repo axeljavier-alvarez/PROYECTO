cf300225b108c50db18af8b2558b93ecc1f2542a
Axel Alvarez <axjavier4alvarez44@gmail.com>
2024-04-18 09:43:46 -0600

    update CRUD

--- Principal.cpp
+++ Principal.cpp
@@ -14,14 +14,13 @@ struct Traductor{
 	char funcionalidad[250];
 };
 
+/* DECLARAR LOS METODOS LEER, CREAR, ACTUALIZAR Y BORRAR*/
 // metodo de leer
 void Leer();
-// metodo
+// metodo de crear
 void Crear();
-
-void Actualizar();
-
-void Borrar();
+void Actualizar(); //Metodo Actualizar.
+void Borrar(); //Metodo Borrar.
 
 // MENU CON LAS OPCIONES ORDENADAS
 main(){
@@ -142,91 +141,56 @@ void Crear(){
 }
 
 void Actualizar(){
-	// r+b abre fichero para escritura, actualizar crea y borra
-	FILE * archivo = fopen(nombre_archivo, "r+b");
-	// declarar
+	FILE * archivo = fopen(nombre_archivo, "r+b"); // Lectura o escritura de un archivo binario.
 	Traductor traductor;
-	
-	// preguntarle por el id
 	int id=0;
-	
-	Leer(); 
-	
 	cout<<"Ingrese el ID que desea Modificar: ";
 	cin>>id;
-	// posicionar mi puntero fseek para posicionar puntero en una determinada posicion
+    fseek(archivo,id * sizeof(Traductor),SEEK_SET); // Posición en una linea especificamente.
 	
-	// BUSCAR POR EL ID
-	fseek(archivo,id * sizeof(Traductor), SEEK_SET);
-	
-		cin.ignore();
-		
-		// Crear(); y actualizar();
-		
-		// necesitamos el get de todo
 		cout<<"Ingrese la Palabra: ";
-		cin.getline(traductor.palabras, 20);
+		cin>>traductor.palabras;
+		cin.ignore();
 		
 		cout<<"Ingrese la traduccion de la palabra: ";
 		cin.getline(traductor.traduccion, 35);
 		
-		cout<<"Ingrese su Funcionalidad: ";
+		cout<<"Ingrese su funcionalidad: ";
 		cin.getline(traductor.funcionalidad, 250);
 		
-		// tenemos que escribir los registros
-		// bandera default 1
 		fwrite(&traductor,sizeof(Traductor), 1, archivo);
-	// cerrar el archivo
-	fclose(archivo);	
-	// mandar a llamar a leer
+		
+	fclose(archivo);
 	Leer();
 }
 
 void Borrar(){
-	const char * nombre_archivo_temp = "archivo_temp.dat";
-	FILE * archivo = fopen(nombre_archivo, "rb");
+	const char *nombre_archivo_temp = "archivo_temp.dat";
 	FILE * archivo_temp = fopen(nombre_archivo_temp, "w+b");
-	
+	FILE * archivo = fopen(nombre_archivo, "rb");
 	Traductor traductor;
 	
-	int id=0, id_n=0;
-	
-	cout<<"Ingrese el ID a eliminar:";
+	int id=0,id_n=0;
+	cout<<"Ingrese el ID a eliminar: ";
 	cin>>id;
 	
-	// recorrer el archivo principal
 	while(fread(&traductor,sizeof(Traductor), 1, archivo)){
-		
-	// en el archivo nuevo agrego todos
-	// menos el que quiero que se elimine
-		if(id_n != id){
-		    fwrite(&traductor,sizeof(Traductor), 1, archivo_temp);
-
-		}
-	// se incrementa
-		id_n++;
+	    	if (id_n!=id){
+	    		fwrite(&traductor,sizeof(Traductor), 1, archivo_temp);
+	    }
+	    id_n++;
 	}
-	
-    // archivo extra para eliminar algun dato
 	fclose(archivo);
 	fclose(archivo_temp);
 	
-	// tiene los datos que quiero en el original
 	archivo_temp = fopen(nombre_archivo_temp, "rb");
-	// luego escribir dentro del archivo permanente
 	archivo = fopen(nombre_archivo, "wb");
 	
-	// recorrido a archivo_temp
 	while(fread(&traductor,sizeof(Traductor), 1, archivo_temp)){
-		
-		// escribo en mi archivo
-		    fwrite(&traductor,sizeof(Traductor), 1, archivo);
+		fwrite(&traductor,sizeof(Traductor), 1, archivo);
 }
-
-    fclose(archivo);
+	fclose(archivo);
 	fclose(archivo_temp);
-	
-	
-	Leer(); 
-
+	Leer();
 }
+
---
+++ README.md
@@ -0,0 +1,21 @@
+### Features
+Proyecto de la universidad que realiza lo siguiente:
+
+- Programa que permite agregar palabras en un archivo, con su respectiva traducción y funcionalidad:
+Ingrese Palabras: if
+Ingrese Traducción: si 
+Ingrese Funcionalidad: es una estructura de control que valida una condición y nos permiten determinar qué acciones tomar dada o no cierta condición.
+
+- Dicho archivo deberá de contener un listado mínimo de estas palabras:
+asm, auto, bool, break, case, catch, char, class, const, const_cast, continue, default, delete, do, double, dynamic_cast, else, enum, explicit, extern, false, float, for, friend, goto, if, inline, int, long, mutable, namespace, new, operator, private, protected, public, register, reinterpret_cast, return, short, signed, sizeof, static, static_cast, struct, switch, template, this, throw, true, try, typedef, typeid, 
+typename, union, unsigned, using, virtual, void, volatile, while
+
+- El programa me debe permitir hacer un CRUD (Crear, Leer, Actualizar y Borrar) del listado de palabras que estoy elaborando en el archivo.
+
+- Crear un programa que me permita ingresar texto multilíneas y me permita traducir una porción de código escrito en c++ , tomando como base las palabras definidas.
+
+## Developed with
+<p align=left>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png" width="100" title="hover text">
+</p>
+
