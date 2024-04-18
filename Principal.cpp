06c7662287e3977cec64c79f59c086dde8588c9a
unknown <psegura@miumg.edu.gt>
2024-04-18 04:34:11 -0600

    Metodos actualizar y borrar, con 16 palabras agregadas.

--- Principal.cpp
+++ Principal.cpp
@@ -17,9 +17,10 @@ struct Traductor{
 /* DECLARAR LOS METODOS LEER, CREAR, ACTUALIZAR Y BORRAR*/
 // metodo de leer
 void Leer();
-// metodo
+// metodo de crear
 void Crear();
-
+void Actualizar(); //Metodo Actualizar.
+void Borrar(); //Metodo Borrar.
 
 // MENU CON LAS OPCIONES ORDENADAS
 main(){
@@ -44,10 +45,10 @@ main(){
                 Crear();
                 break;
             case 3:
-                // Actualizar();
+                Actualizar();
                 break;
             case 4:
-                // Borrar();
+                Borrar();
                 break;
             case 0:
                 cout << "Ha salido del programa";
@@ -136,6 +137,60 @@ void Crear(){
 	
 	
 	// mandar a llamar a leer
+	Leer();
+}
+
+void Actualizar(){
+	FILE * archivo = fopen(nombre_archivo, "r+b"); // Lectura o escritura de un archivo binario.
+	Traductor traductor;
+	int id=0;
+	cout<<"Ingrese el ID que desea Modificar: ";
+	cin>>id;
+    fseek(archivo,id * sizeof(Traductor),SEEK_SET); // Posición en una linea especificamente.
+	
+		cout<<"Ingrese la Palabra: ";
+		cin>>traductor.palabras;
+		cin.ignore();
+		
+		cout<<"Ingrese la traduccion de la palabra: ";
+		cin.getline(traductor.traduccion, 35);
+		
+		cout<<"Ingrese su funcionalidad: ";
+		cin.getline(traductor.funcionalidad, 250);
+		
+		fwrite(&traductor,sizeof(Traductor), 1, archivo);
+		
+	fclose(archivo);
+	Leer();
+}
+
+void Borrar(){
+	const char *nombre_archivo_temp = "archivo_temp.dat";
+	FILE * archivo_temp = fopen(nombre_archivo_temp, "w+b");
+	FILE * archivo = fopen(nombre_archivo, "rb");
+	Traductor traductor;
+	
+	int id=0,id_n=0;
+	cout<<"Ingrese el ID a eliminar: ";
+	cin>>id;
+	
+	while(fread(&traductor,sizeof(Traductor), 1, archivo)){
+	    	if (id_n!=id){
+	    		fwrite(&traductor,sizeof(Traductor), 1, archivo_temp);
+	    }
+	    id_n++;
+	}
+	fclose(archivo);
+	fclose(archivo_temp);
+	
+	archivo_temp = fopen(nombre_archivo_temp, "rb");
+	archivo = fopen(nombre_archivo, "wb");
+	
+	while(fread(&traductor,sizeof(Traductor), 1, archivo_temp)){
+		fwrite(&traductor,sizeof(Traductor), 1, archivo);
+}
+	fclose(archivo);
+	fclose(archivo_temp);
 	Leer();
 }
 
